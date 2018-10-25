3172a0f07691667832dadd2627f01bdd8aa0df7e
Vivek Gnanavelu <g.vivek14@gmail.com>
2018-10-24 21:42:21 -0700

    Basic webapp with sample homepage

---
+++ webapp/app/app/main.py
@@ -0,0 +1,33 @@
+import os
+import json
+import uuid
+import time
+from bson.json_util import dumps
+from flask import Flask, request, abort, jsonify, logging, render_template
+from pymongo import MongoClient
+import requests
+from requests.auth import HTTPBasicAuth
+from flask_basicauth import BasicAuth
+
+app = Flask(__name__)
+
+
+# app.config['BASIC_AUTH_USERNAME'] = 'ese'
+# app.config['BASIC_AUTH_PASSWORD'] = 'ExpertSearchEngine'
+
+# enforcing authentication on entire app
+# app.config['BASIC_AUTH_FORCE'] = True
+
+# basic_auth = BasicAuth(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pass
+    return render_template('home.html')
+
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
