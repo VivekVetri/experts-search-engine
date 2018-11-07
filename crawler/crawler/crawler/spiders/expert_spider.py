37119c8811e39237e5623bd43b1a761de2b02c70
Nitya Ganesan <nityag2@illinois.edu>
2018-11-07 10:46:31 -0700

    Added download_delay Logic to scrape data for MIT CS faculty

--- crawler/crawler/crawler/spiders/expert_spider.py
+++ crawler/crawler/crawler/spiders/expert_spider.py
@@ -3,15 +3,30 @@ import scrapy
 
 class ExpertSpider(scrapy.Spider):
     name = "expert"
+    download_delay = 5
     start_urls = [
-        'https://www.csail.mit.edu/person/costis-daskalakis',
+        # MIT Faculty & Advisors II - Computer Science (Theory)
+        'https://www.eecs.mit.edu/people/faculty-advisors/35',
+        # MIT Faculty & Advisors II - Computer Science (Systems)
+        'https://www.eecs.mit.edu/people/faculty-advisors/32',
+        # MIT Faculty & Advisors II - Computer Science (Artificial Intelligence)
+        'https://www.eecs.mit.edu/people/faculty-advisors/34',
     ]
 
     def parse(self, response):
-        name = response.css('title::text').extract_first()
+        # Follow links to each researcher's MIT web page
+        faculty = response.css('div.people-list')[0]
+        if faculty is not None:
+            for href in faculty.css('span.field-content.card-title a::attr(href)'):
+                yield response.follow(href, self.parse_faculty)
+
+    def parse_faculty(self, response):
+        page = response.url
+        name = response.css('head title::text').extract_first()
         details = response.css('p').extract()
         yield {
+            'page': page,
             'name': name,
-            'details': details
+            'details': details,
         }
 
