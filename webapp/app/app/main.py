3c02944ac394e30d0d1f9a9410c721ff31cb4235
Vivek Gnanavelu <g.vivek14@gmail.com>
2018-11-15 10:47:37 -0800

    Initial integration of cranfield

--- webapp/app/app/main.py
+++ webapp/app/app/main.py
@@ -1,3 +1,5 @@
+import time
+
 import metapy
 import pytoml
 from flask import Flask, request, abort, jsonify, logging, render_template
@@ -17,23 +19,44 @@ app = Flask(__name__)
 
 # basic_auth = BasicAuth(app)
 
+def load_ranker(cfg_file):
+    """
+    Use this function to return the Ranker object to evaluate.
+    The parameter to this function, cfg_file, is the path to a
+    configuration file used to load the index. You can ignore this for MP2.
+    """
+    # You can set your new InL2Ranker here by: return InL2Ranker(some_param=1.0)
+    # Try to set the value between 0.9 and 1.0 and see what performs best
+    # return metapy.index.JelinekMercer(0.09)
+    # return metapy.index.AbsoluteDiscount(0.01)
+    return metapy.index.DirichletPrior()
+    # return InL2Ranker(some_param=0.9875)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-
+    path_prefix = "/app/backend/"
     if request.method == 'POST':
         pass
 
-    cfg = "/app/backend/config.toml"
+    start_time = time.time()
+    cfg = path_prefix + "config.toml"
     with open(cfg, 'r') as fin:
         cfg_d = pytoml.load(fin)
+
+    idx = metapy.index.make_inverted_index(cfg)
+    ranker = load_ranker(cfg)
+    ev = metapy.index.IREval(cfg)
+
     query_cfg = cfg_d['query-runner']
     query_path = query_cfg.get('query-path', 'queries.txt')
     query_start = query_cfg.get('query-id-start', 0)
 
+    top_k = 10
+
     query = metapy.index.Document()
     print('Running queries')
-    with open('inl2.avg_p.txt', 'a') as file:
+    with open(path_prefix + 'inl2.avg_p.txt', 'a') as file:
         with open(query_path) as query_file:
             for query_num, line in enumerate(query_file):
                 query.content(line.strip())
