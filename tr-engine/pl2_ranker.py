a695b7b9e53537ba0b7b2ef0007a417a57457c3a
Vivek Gnanavelu <g.vivek14@gmail.com>
2018-12-03 15:52:02 -0800

    BM25 and PL2 rankers

--- tr-engine/pl2_ranker.py
+++ tr-engine/pl2_ranker.py
@@ -1,6 +1,30 @@
+import math
 import sys
+import time
 
 import metapy
+
+
+class PL2Ranker(metapy.index.RankingFunction):
+    """
+    Create a new ranking function in Python that can be used in MeTA
+    """
+
+    def __init__(self, c_param=0.5):
+        self.c = c_param
+        super(PL2Ranker, self).__init__()
+
+    def score_one(self, sd):
+        lda = sd.num_docs / sd.corpus_term_count
+        tfn = sd.doc_term_count * math.log2(1.0 + self.c * sd.avg_dl /
+                                            sd.doc_size)
+        if lda < 1 or tfn <= 0:
+            return 0.0
+        numerator = tfn * math.log2(tfn * lda) \
+                    + math.log2(math.e) * (1.0 / lda - tfn) \
+                    + 0.5 * math.log2(2.0 * math.pi * tfn)
+        return sd.query_term_weight * numerator / (tfn + 1.0)
+
 
 if __name__ == '__main__':
     config = sys.argv[1]
@@ -9,12 +33,13 @@ if __name__ == '__main__':
 
     print("No. of documents : ", idx.num_docs())
     print("No. of unique terms : ", idx.unique_terms())
-    print("Avg document length : ",  idx.avg_doc_length())
+    print("Avg document length : ", idx.avg_doc_length())
     print("Total corpus terms : ", idx.total_corpus_terms())
 
     ranker = metapy.index.OkapiBM25()
     query = metapy.index.Document()
     query.content('computer network')
+    ranker = PL2Ranker()
     top_docs = ranker.score(idx, query, num_results=5)
     print(top_docs)
 
